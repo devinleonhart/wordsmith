d06568e5749c2b25375ece75572fd5f9887c072c
Devin Leonhart <devinleonhart@fastmail.com>
2020-05-08 19:32:45 -0700

    Update prefix by environment.

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,17 @@
 const environment = process.env.NODE_ENV;
 
-const settings = {
-  prefix: "/ws",
-};
+const settings = {};
 
 if (environment === "production") {
   settings.DISCORD_SECRET_KEY_WS = process.env.DISCORD_SECRET_KEY_WS;
+  settings.prefix = "/ws";
 } else if (environment === "test") {
   settings.DISCORD_SECRET_KEY_WS = "dummy key";
+  settings.prefix = "/ws";
 } else if (environment === "development") {
   require("dotenv").config();
   settings.DISCORD_SECRET_KEY_WS = process.env.DISCORD_SECRET_KEY_WS;
+  settings.prefix = "/wst";
 }
 
 module.exports = settings;
