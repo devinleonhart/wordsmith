a488c20c91af5a8a1676dbe7a2dfdc8feb9a9126
Devin Leonhart <devinleonhart@fastmail.com>
2021-07-03 17:29:54 -0700

    Remove tests for reddis.

--- test/handlers/rr.test.js
+++ test/handlers/rr.test.js
@@ -4,29 +4,6 @@ const expect = chai.expect;
 const proxyquire = require("proxyquire").noCallThru();
 const handlers = require("../../src/handlers").default;
 
-describe("handlers for the s command", function() {
-  describe("when the inputs are valid", function() {
-    let rollSpy;
-    let stubbedHandler;
+describe("handlers for the rr command", function() {
 
-    beforeEach(function() {
-      rollSpy = sinon.spy();
-      stubbedHandler = proxyquire("../../src/handlers", {
-        "../rules": { useStar: rollSpy },
-      }).default;
-    });
-
-    it("passes the correct inputs to rules.useStar()", function() {
-      stubbedHandler({}, "s", ["Devin"]);
-      expect(rollSpy.calledOnce).to.be.true;
-      expect(rollSpy.calledWithMatch("Devin")).to.be.true;
-    });
-  });
-
-  describe("when the inputs aren't valid", function() {
-    it("throws with a listing of the expected parameters", function() {
-      const requestWithTooFewArgs = () => handlers({}, "s", [], () => {});
-      expect(requestWithTooFewArgs).to.throw("{Character Name}");
-    });
-  });
 });
