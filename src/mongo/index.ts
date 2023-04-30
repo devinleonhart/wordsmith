4befc6d50e6173e145ea80b86146fc8b8490e484
Devin Leonhart <devinleonhart@fastmail.com>
2023-04-29 19:03:56 -0700

    Remove listModels

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -1,25 +1,14 @@
 import mongoose from "mongoose";
 import settings from "../settings";
-import { Character, Game } from "./models";
 
 const uri = settings.mongoConnectionURI;
 
 async function setupMongo() {
   try {
     await mongoose.connect(uri);
-    await listModels();
-
   } catch(err) {
     console.error(err);
   }
-}
-
-async function listModels(){
-  const characters = await Character.find();
-  const games = await Game.find();
-
-  console.log("Characters:", characters);
-  console.log("Game:", games);
 }
 
 export { setupMongo };
