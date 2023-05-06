82107ee9a7a376847a974baeb6b5969c476ac3b8
Devin Leonhart <devinleonhart@fastmail.com>
2023-05-06 13:20:27 -0700

    Add user select menu for switch GM.

--- src/commands/game/switchGM.ts
+++ src/commands/game/switchGM.ts
@@ -1,29 +1,27 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
+import {
+  SlashCommandBuilder,
+  UserSelectMenuBuilder,
+  ActionRowBuilder,
+} from "@discordjs/builders";
 import { CommandInteraction } from "discord.js";
-import { switchGM } from "../../mongo/helpers";
 
 const commandName = "switch-gm";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName(commandName)
-    .setDescription("Make another use the gm of this game.")
-    .addUserOption((option) =>
-      option
-        .setName("user")
-        .setDescription("The user who will become the gm.")
-        .setRequired(true)
-    ),
+    .setDescription("Switch the GM!"),
   async execute(interaction: CommandInteraction) {
-    const sco: SlashCommandOptions = {
-      playerID: interaction.user.id,
-      discordChannelID: interaction.channelId,
-      options: {
-        user: interaction.options.get("user")?.value as string,
-      },
-    };
+    const menu = new UserSelectMenuBuilder()
+      .setCustomId("switch-gm-menu")
+      .setMinValues(1)
+      .setMaxValues(1)
+      .setPlaceholder("Select the new gm!");
 
-    await switchGM(sco);
-    await interaction.reply("GM swapped!");
+    await interaction.reply({
+      components: [
+        new ActionRowBuilder<UserSelectMenuBuilder>().addComponents(menu),
+      ],
+    });
   },
 };
--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -6,31 +6,40 @@ module.exports = {
   async execute(interaction: Interaction, client: Client) {
     if (interaction.isChatInputCommand()) {
       const command = client.commands.get(interaction.commandName);
-
       if (!command) return;
-
-      try {
-        await command.execute(interaction, client);
-      } catch (error) {
-        if (error instanceof WordsmithError) {
-          await interaction.reply({
-            content: `${error.message}`,
-            ephemeral: true,
-          });
-        }
-        // Do nothing with any other error type. We do not wish to return unknown errors to the client.
-      }
+      handleExecute(command, interaction, client);
     } else if (interaction.isStringSelectMenu()) {
       const { selectMenus } = client;
       const { customId } = interaction;
       const menu = selectMenus.get(customId);
-      if (!menu) throw new Error("There is no code for this selectMenu.");
-
-      try {
-        await menu.execute(interaction, client);
-      } catch (error) {
-        console.error(error);
-      }
+      if (!menu) throw new Error("No user selectMenu command found!");
+      handleExecute(menu, interaction, client);
+    } else if (interaction.isUserSelectMenu()) {
+      const { selectMenus } = client;
+      const { customId } = interaction;
+      const menu = selectMenus.get(customId);
+      if (!menu) throw new Error("No user selectMenu command found!");
+      handleExecute(menu, interaction, client);
+    } else if (interaction.isContextMenuCommand()) {
+      const { commands } = client;
+      const { commandName } = interaction;
+      const contextCommand = commands.get(commandName);
+      if (!contextCommand) throw new Error("No console command found!");
+      handleExecute(contextCommand, interaction, client);
     }
   },
 };
+
+async function handleExecute(instance: any, interaction: any, client: any) {
+  try {
+    await instance.execute(interaction, client);
+  } catch (error) {
+    if (error instanceof WordsmithError) {
+      await interaction.reply({
+        content: `${error.message}`,
+        ephemeral: true,
+      });
+    }
+    // Do nothing with any other error type. We do not wish to return unknown errors to the client.
+  }
+}
