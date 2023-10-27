0e8da25bb004f2145ce0431252c04e7ae8c1c56e
Devin Leonhart <devinleonhart@fastmail.com>
2023-10-27 15:46:40 -0700

    Add two logs.

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -5,6 +5,8 @@ const uri = settings.mongoConnectionURI;
 
 async function setupMongo() {
   try {
+    console.log("Connecting to MongoDB...")
+    console.log(uri)
     await mongoose.connect(uri);
   } catch (err) {
     console.error(err);
